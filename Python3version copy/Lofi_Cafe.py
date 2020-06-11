e6cefca29f61454d7665cf14b61def17205828b6
Robert L West <rlwest@gmail.com>
2020-06-11 15:06:22 -0400

    nicer code

--- Python3version copy/Lofi_Cafe.py
+++ Python3version copy/Lofi_Cafe.py
@@ -9,28 +9,29 @@ sys.path.append('/Users/robertwest/pythonACTR3-master/')
 import ccm
 from ccm.lib.actr import *
 
-from Barista_Dave import Barista_Dave
-from Coffee_Machine import Coffee_Machine
-
-# get objects
+# get environment elements
 from Objects import Objects
 from Coffee_Machine import Coffee_Machine_Objects
 
-# assemble objects into environment 
+# put objects together
 Lofi_Cafe = type('Lofi_Cafe', (Objects,Coffee_Machine_Objects), dict(lofi='lofi'))
 
-######## run model #########
-
-#log = ccm.log()
-dave = Barista_Dave()
-coffee_machine = Coffee_Machine()
+# create environment
 lofi_cafe = Lofi_Cafe()
 
+# get agent descriptions
+from Barista_Dave import Barista_Dave
+from Coffee_Machine import Coffee_Machine
 
+# create agents for environment
+dave = Barista_Dave()
+coffee_machine = Coffee_Machine()
+
+# put agents into environment
 lofi_cafe.agent = dave
 lofi_cafe.agent = coffee_machine 
 
-
+# run
 ccm.log_everything(lofi_cafe)
 lofi_cafe.run()
 ccm.finished()
